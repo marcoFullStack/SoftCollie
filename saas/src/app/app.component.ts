a0450d46c068943c7bfe958fa559af36d665767c
Marco Gomez Zazo <mgz5489@hotmail.com>
2025-05-04 22:37:52 +0200

    Add files via upload

---
+++ saas/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component }                                                                          from '@angular/core';
+import { RouterModule }                                                                       from '@angular/router';
+import { ScMapHighlightAreaComponent, ScMapHighlightBrushComponent, ScMapHighlightComponent } from "@softcollie/sc-map-highlight";
+
+@Component({
+    imports: [RouterModule, ScMapHighlightComponent, ScMapHighlightAreaComponent, ScMapHighlightBrushComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'saas';
+}
